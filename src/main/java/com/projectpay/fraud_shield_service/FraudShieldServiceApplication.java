ecfeab50e6ac982c0d61119808d4cd812b516c9d
sajal <sajal.mainhuna@gmail.com>
2026-05-26 21:49:28 +0530

    Initial Commit

---
+++ src/main/java/com/projectpay/fraud_shield_service/FraudShieldServiceApplication.java
@@ -0,0 +1,13 @@
+package com.projectpay.fraud_shield_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudShieldServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudShieldServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectpay/fraud_shield_service/FraudShieldServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectpay.fraud_shield_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudShieldServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
